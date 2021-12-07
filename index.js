579e0df569f1549bc8708f6f64132173c0491d06
Ajna Moreira <86033670+AjnaMoreira@users.noreply.github.com>
2021-12-07 15:15:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { SistemaAutenticacao } from './SistemaAutenticacao.js'
 
 const diretor = new Diretor('Joana', 10000, 12345678900)
 diretor.cadastarSenha('123456')
-const gerente = new Gerente('Ajna', 5000, 98765432100)
+const gerente = new Gerente('Maria', 5000, 98765432100)
 gerente.cadastarSenha('123')
 
 const cliente = new Cliente('Lara', 85698745832, '456')
