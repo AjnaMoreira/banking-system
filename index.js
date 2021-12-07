d8fca3c9fae710f7f63e07b09e86e55add6710cb
Ajna Moreira <86033670+AjnaMoreira@users.noreply.github.com>
2021-12-07 15:15:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Cliente } from './Cliente.js'
+import { Gerente } from './Funcionario/Gerente.js'
+import { Diretor } from './Funcionario/Diretor.js'
+import { SistemaAutenticacao } from './SistemaAutenticacao.js'
+
+const diretor = new Diretor('Joana', 10000, 12345678900)
+diretor.cadastarSenha('123456')
+const gerente = new Gerente('Ajna', 5000, 98765432100)
+gerente.cadastarSenha('123')
+
+const cliente = new Cliente('Lara', 85698745832, '456')
+
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123')
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456')
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456')
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado)
